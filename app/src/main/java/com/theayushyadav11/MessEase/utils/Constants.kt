8b2a44a32ffecb85c27b062f458bc5e7eaf9b7e6
Om Swami <reach.oms2004@gmail.com>
2026-03-22 16:02:13 +0530

    chore: fix updates for playstore

--- app/src/main/java/com/theayushyadav11/MessEase/utils/Constants.kt
+++ app/src/main/java/com/theayushyadav11/MessEase/utils/Constants.kt
@@ -126,6 +126,18 @@ class Constants {
             return dateFormat.format(date)
         }
 
+        fun isVersionGreater(remote: String, local: String): Boolean {
+            val remoteParts = remote.split(".").map { it.toIntOrNull() ?: 0 }
+            val localParts = local.split(".").map { it.toIntOrNull() ?: 0 }
+            val length = maxOf(remoteParts.size, localParts.size)
+            for (i in 0 until length) {
+                val r = remoteParts.getOrNull(i) ?: 0
+                val l = localParts.getOrNull(i) ?: 0
+                if (r > l) return true
+                if (r < l) return false
+            }
+            return false
+        }
 
     }
 }
